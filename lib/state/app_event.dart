5bf03fbce84e6e6508422655bd507c99c789411f
Carrrlll007 <carlo.dortenzi@student.kuleuven.be>
2026-02-08 13:21:00 +0100

    Update

--- lib/state/app_event.dart
+++ lib/state/app_event.dart
@@ -7,40 +7,40 @@ sealed class AppEvent {
 
 class InterferenceDetected extends AppEvent {
   final int intensity;
-  const InterferenceDetected({required super.at, this.intensity = 1});
+  const InterferenceDetected({required DateTime at, this.intensity = 1}) : super(at);
 }
 
 class ForgeStarted extends AppEvent {
   final Quest quest;
-  const ForgeStarted({required super.at, required this.quest});
+  const ForgeStarted({required DateTime at, required this.quest}) : super(at);
 }
 
 class ForgeSuccess extends AppEvent {
   final Quest quest;
-  const ForgeSuccess({required super.at, required this.quest});
+  const ForgeSuccess({required DateTime at, required this.quest}) : super(at);
 }
 
 class ForgeAbort extends AppEvent {
-  const ForgeAbort({required super.at});
+  const ForgeAbort({required DateTime at}) : super(at);
 }
 
 class QuestCompleted extends AppEvent {
   final Quest quest;
-  const QuestCompleted({required super.at, required this.quest});
+  const QuestCompleted({required DateTime at, required this.quest}) : super(at);
 }
 
 class RecoveryTriggered extends AppEvent {
-  const RecoveryTriggered({required super.at});
+  const RecoveryTriggered({required DateTime at}) : super(at);
 }
 
 class StabilityAchieved extends AppEvent {
-  const StabilityAchieved({required super.at});
+  const StabilityAchieved({required DateTime at}) : super(at);
 }
 
 class EndgameReached extends AppEvent {
-  const EndgameReached({required super.at});
+  const EndgameReached({required DateTime at}) : super(at);
 }
 
 class ManualReset extends AppEvent {
-  const ManualReset({required super.at});
+  const ManualReset({required DateTime at}) : super(at);
 }
